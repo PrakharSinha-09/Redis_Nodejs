5cc1932774851d45ad5bbd15cd006ee140a126b0
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-05-21 00:00:07 +0530

    adding the server code and the worker code

---
+++ express-backend/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { createClient } from "redis";
+
+const app = express();
+app.use(express.json());
+
+const client = createClient();
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+app.post("/submit", async (req, res) => {
+    const problemId = req.body.problemId;
+    const code = req.body.code;
+    const language = req.body.language;
+
+    try {
+        await client.lPush("submissions", JSON.stringify({ code, language, problemId }));     //the first arg i.e., "submissions" is the name of the queue!... Commit to Memory!
+        // Store in the database... ignoring for now
+        res.status(200).send("Submission received and stored.");
+    } catch (error) {
+        console.error("Redis error:", error);
+        res.status(500).send("Failed to store submission.");
+    }
+});
+
+async function startServer() {
+    try {
+        await client.connect();                         //redis connection
+        console.log("Connected to Redis");
+
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
+    }
+}
+
+startServer();
